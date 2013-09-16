98ad5d96f6a0a046abfe80f0cb4b3d956428d17f
brianmcn <lorgon1@yahoo.com>
2013-09-16 10:59:52 -0700

    more decoding of block data to understand how things work

--- MMM.fs
+++ MMM.fs
@@ -46,6 +46,27 @@ and NBT =
     | List of Name * Payload // (name,kind,num,a)
     | Compound of Name * NBT[]
     | IntArray of Name * int[] // int32 before data shows num elements
+    member this.Name =
+        match this with
+        | End -> "End"
+        | Byte(n,_) -> n
+        | Short(n,_) -> n
+        | Int(n,_) -> n
+        | Long(n,_) -> n
+        | Float(n,_) -> n
+        | Double(n,_) -> n
+        | ByteArray(n,_) -> n
+        | String(n,_) -> n
+        | List(n,_) -> n
+        | Compound(n,_) -> n
+        | IntArray(n,_) -> n
+    member this.Item(s:string) =
+        match this with
+        | Compound(_n,a) ->
+            match a |> Array.tryFind (fun x -> x.Name = s) with
+            | Some x -> x
+            | None -> failwithf "tag %s not found" s
+        | _ -> failwith "try to name-index into a non-compound"
     member this.ToString(prefix) =
         match this with
         | End -> ""
@@ -58,16 +79,16 @@ and NBT =
         | ByteArray(n,_a) -> prefix + n + " : <bytes>"
         | String(n,s) -> prefix + n + " : " + s
         | List(n,pay) -> 
-            let sb = new System.Text.StringBuilder(prefix + n + " : []\n")
+            let sb = new System.Text.StringBuilder(prefix + n + " : [] ")
             let p = "    " + prefix
             match pay with
-            | Bytes(a) -> for x in a do sb.Append(x.ToString(p) + "\n") |> ignore
-            | Shorts(a) -> for x in a do sb.Append(x.ToString(p) + "\n") |> ignore
-            | Ints(a) -> for x in a do sb.Append(x.ToString(p) + "\n") |> ignore
-            | Longs(a) -> for x in a do sb.Append(x.ToString(p) + "\n") |> ignore
-            | Floats(a) -> for x in a do sb.Append(x.ToString(p) + "\n") |> ignore
-            | Doubles(a) -> for x in a do sb.Append(x.ToString(p) + "\n") |> ignore
-            | Compounds(a) -> for c in a do for x in c do sb.Append(x.ToString(p) + "\n") |> ignore
+            | Bytes(a) -> sb.Append(a.Length.ToString() + " bytes\n") |> ignore
+            | Shorts(a) -> sb.Append(a.Length.ToString() + " shorts\n") |> ignore
+            | Ints(a) -> sb.Append(a.Length.ToString() + " ints\n") |> ignore
+            | Longs(a) -> sb.Append(a.Length.ToString() + " longs\n") |> ignore
+            | Floats(a) -> sb.Append(a.Length.ToString() + " floats\n") |> ignore
+            | Doubles(a) -> sb.Append(a.Length.ToString() + " doubles\n") |> ignore
+            | Compounds(a) -> sb.Append("\n") |> ignore; for c in a do for x in c do sb.Append(x.ToString(p) + "\n") |> ignore
             sb.ToString()
         | Compound(n,a) -> 
             let sb = new System.Text.StringBuilder(prefix + n + " :\n")
@@ -161,11 +182,34 @@ type RegionFile private () =
 
 let main() =
     let filename = """F:\.minecraft\saves\BingoGood\region\r.0.0.mca"""
-    // I want to read chunk (11,11), which is coords (192,192) and is the top left of my bingo map
-    let nbt = RegionFile.ReadChunk(filename, 11, 11)
+    // I want to read chunk (12,12), which is coords (192,192) and is the top left of my bingo map
+    let nbt = RegionFile.ReadChunk(filename, 12, 12)
     printfn "%s" (nbt.ToString())
+    let theChunk = match nbt with Compound(_,[|c;_|]) -> c
+    printfn "%s" theChunk.Name 
+    let sections = match theChunk.["Sections"] with List(_,Compounds(cs)) -> cs
+    printfn "%d" sections.Length 
+    // height of map I want to look at is just below 208, which is section 12
+    let s = sections |> Array.find (Array.exists (function Byte("Y",12uy) -> true | _ -> false))
+    for x in s do printfn "%s\n" (x.ToString())
+    let blocks = s |> Array.pick (function ByteArray("Blocks",a) -> Some a | _ -> None)
+    // 76 is active redstone torch blockID; 23 is dispenser
+    printfn "red torch? %A" (blocks |> Array.exists (fun b -> b = 76uy))
+    // dispenser in next higher section (13)... i could see its contents already in the TileEntities of the chunk
+    let s = sections |> Array.find (Array.exists (function Byte("Y",13uy) -> true | _ -> false))
+    let blocks = s |> Array.pick (function ByteArray("Blocks",a) -> Some a | _ -> None)
+    printfn "dispenser? %A" (blocks |> Array.exists (fun b -> b = 23uy))
+    let dispIndex = blocks |> Array.findIndex (fun b -> b = 23uy)
+    let blockData = s |> Array.pick (function ByteArray("Data",a) -> Some a | _ -> None)
+    // expand 2048 half-bytes into 4096 for convenience of same indexing without having to decode YZX
+    let blockData = Array.init 4096 (fun x -> if x%2=1 then blockData.[x/2] >>> 4 else blockData.[x/2] &&& 0xFuy)
+    printfn "disp orientation %d" blockData.[dispIndex]
+(*
+    for i = 0 to 4095 do
+        printf "%d " blocks.[i]
+        if i%16 = 15 then printfn ""
+*)
     ()
 
 printfn "hi"
 main()
-
