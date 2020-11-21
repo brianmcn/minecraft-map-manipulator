06d54e0fe577feebf6b52ea4e8772659ccc1d8d1
brianmcn <lorgon@gmail.com>
2020-11-21 15:08:07 -0500

    per-player storage using PlayerDB

--- MinecraftMapManipulator/Minecraft_1_16/PiggyBag.fs
+++ MinecraftMapManipulator/Minecraft_1_16/PiggyBag.fs
@@ -4,7 +4,10 @@ open IO_Utilities
 
 let NS = "lorgon111"
 
-let makePiggyBag() =   // Note: multi-player is just too tough/tedious/expensive to implement
+let makePiggyBag() =   // Note: multi-player is just too tough/tedious/expensive to implement 
+
+    // Note: uses https://github.com/rx-modules/PlayerDB for storage
+    // TODO temp scoreboard id tags to associate chest and owner perhaps
 
     let pack = new DataPackArchive("""C:\Users\Admin1\AppData\Roaming\.minecraft\saves\test2""", "piggy_bag", "Piggy Bag by Dr. Brian Lorgon111")
 
@@ -24,15 +27,18 @@ let makePiggyBag() =   // Note: multi-player is just too tough/tedious/expensive
         [|
         // keep tracking inv
         """advancement revoke @s only lorgon111:inv_changed"""
-        // detect if Piggy summoning item in upper right of player inv (single player only)
+        // detect if Piggy summoning item in upper right of current player inv
         """data remove storage lorgon111:inv Temp"""
-        """execute if data entity @p Inventory[{Slot:17b,tag:{display:{Name:'{"text":"Piggy Bag"}'}}}] run data modify storage lorgon111:inv Temp set value 1"""
+        """execute if data entity @s Inventory[{Slot:17b,tag:{display:{Name:'{"text":"Piggy Bag"}'}}}] run data modify storage lorgon111:inv Temp set value 1"""
+        // load up per-player data
+        """function rx.playerdb:api/get_self"""
         // if just moved to upper right, summon fresh one
-        """execute unless data storage lorgon111:inv UpperRight run execute if data storage lorgon111:inv Temp run execute as @p at @s run function lorgon111:upper_right"""
+        """execute unless data storage rx:io lorgon111.UpperRight run execute if data storage lorgon111:inv Temp run execute at @s run function lorgon111:upper_right"""
         // if just moved away from upper right, kill any
-        """execute if data storage lorgon111:inv UpperRight run execute unless data storage lorgon111:inv Temp run function lorgon111:not_upper_right"""
+        """execute if data storage rx:io lorgon111.UpperRight run execute unless data storage lorgon111:inv Temp run function lorgon111:not_upper_right"""
         |])
 
+    // TODO
     pack.WriteFunction(NS, "kill",
         [|
         // kill would loot the chest items, which is not desirable.
@@ -43,29 +49,52 @@ let makePiggyBag() =   // Note: multi-player is just too tough/tedious/expensive
 
     pack.WriteFunction(NS, "master",
         [|
-        // if any piggybag is not near the player (single player), kill them all to ensure don't duplicate 
+        """execute as @e[type=chest_minecart,tag=piggybag] at @s run function lorgon111:per_piggy"""
+        |])
+
+    pack.WriteFunction(NS, "per_piggy",
+        [|
+        // TODO multiplayer
+        // if piggybag is not near the player (single player), kill them all to ensure don't duplicate 
         // (e.g. if tp to new chunks & unload/reload old entity with older chest contents)
-        """execute at @p as @e[type=chest_minecart,tag=piggybag,distance=10..] run function lorgon111:kill"""
+        """execute unless entity @a[distance=..9] run function lorgon111:kill"""
         // if piggybag exists near the player (single player), copy chest contents to storage Inv
-        """execute at @p as @e[type=chest_minecart,tag=piggybag,distance=..9,limit=1,sort=nearest] run data modify storage lorgon111:inv Inv set from entity @s Items"""
+        """execute if entity @a[distance=..9] run function lorgon111:copy_to_storage"""
+        |])
+
+    pack.WriteFunction(NS, "copy_to_storage",
+        [|
+        // while the piggy is still @s, copy its data to temp storage
+        """data modify storage lorgon111:inv Inv set from entity @s Items"""
+        // TODO find owning player
+        // copy temp storage to player storage
+        """execute as @p run function lorgon111:copy_to_player_storage"""
+        |])
+
+    pack.WriteFunction(NS, "copy_to_player_storage",
+        [|
+        // load up per-player data
+        """function rx.playerdb:api/get_self"""
+        // copy storage
+        """data modify storage rx:io lorgon111.Inv set from storage lorgon111:inv Inv"""
         |])
 
     pack.WriteFunction(NS, "not_upper_right",
         [|
-        """data remove storage lorgon111:inv UpperRight"""
+        """data remove storage rx:io lorgon111.UpperRight"""
         """function lorgon111:kill"""
         |])
 
+    // TODO
     pack.WriteFunction(NS, "upper_right",
         [|
-        // (as and at the player @p)
-        """data modify storage lorgon111:inv UpperRight set value 1"""
+        """data modify storage rx:io lorgon111.UpperRight set value 1"""
         """function lorgon111:kill"""
         // summon a new piggybag
         """execute positioned ~ ~-0.7 ~ anchored eyes run summon armor_stand ^ ^ ^1.3 {Invisible:1b,NoGravity:1b,Marker:1b,Invulnerable:1b,Tags:["piggybag"],Passengers:[{id:"chest_minecart",CustomName:'{"text":"Piggy Bag"}',Invulnerable:1b,Tags:["piggybag"]}]}"""
         """playsound minecraft:entity.pig.ambient neutral @p ~ ~ ~ 1"""
         // copy data from storage Inv to chest contents
-        """data modify entity @e[type=chest_minecart,limit=1,sort=nearest,tag=piggybag] Items set from storage lorgon111:inv Inv"""
+        """data modify entity @e[type=chest_minecart,limit=1,sort=nearest,tag=piggybag] Items set from storage rx:io lorgon111.Inv"""
         |])
 
     // TODO other stuff from minecraft/recipes and tags/items/log? or in a separate pack?
